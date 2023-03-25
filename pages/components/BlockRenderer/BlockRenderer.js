f66f8fbed016125012ed7f3b954bf711f08487ff
Jean Felisme <jeazy@bizdotsmedia.com>
2023-03-24 22:38:37 -0400

    adding heading and inner block

--- pages/components/BlockRenderer/BlockRenderer.js
+++ pages/components/BlockRenderer/BlockRenderer.js
@@ -1,8 +1,26 @@
+import { Cover } from "../Cover/index";
+import { Heading } from "../Heading";
+
 export const BlockRenderer = ({ blocks }) => {
   return blocks.map((block) => {
     switch (block.name) {
       case "core/cover": {
-        return <div key={block.id}>Core Cover</div>;
+        console.log("block: ", block);
+        return (
+          <Cover key={block.id} background={block.attributes.url}>
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Cover>
+        );
+      }
+      case "core/heading": {
+        return (
+          <Heading
+            key={block.id}
+            textAlign={block.attributes.textAlign}
+            level={block.attributes.level}
+            content={block.attributes.content}
+          />
+        );
       }
       default:
         return null;
