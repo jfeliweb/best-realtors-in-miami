30bcf567f70242e42f3bc3d45707a7c5fe20eee9
Jean Felisme <jeazy@bizdotsmedia.com>
2023-03-25 00:34:45 -0400

    setup of Paragraph Block

--- pages/components/BlockRenderer/BlockRenderer.js
+++ pages/components/BlockRenderer/BlockRenderer.js
@@ -1,5 +1,7 @@
 import { Cover } from "../Cover/index";
 import { Heading } from "../Heading";
+import { Paragraph } from "../Paragraph";
+import { theme } from "theme";
 
 export const BlockRenderer = ({ blocks }) => {
   return blocks.map((block) => {
@@ -19,6 +21,19 @@ export const BlockRenderer = ({ blocks }) => {
             textAlign={block.attributes.textAlign}
             level={block.attributes.level}
             content={block.attributes.content}
+          />
+        );
+      }
+      case "core/paragraph": {
+        return (
+          <Paragraph
+            key={block.id}
+            textAlign={block.attributes.align}
+            content={block.attributes.content}
+            textColor={
+              theme[block.attributes.textColor] ||
+              block.attributes.style?.color?.text
+            }
           />
         );
       }
