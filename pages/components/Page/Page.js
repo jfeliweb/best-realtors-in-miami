cbf7b519395b213f90715116b8cf3eea64edf333
Jean Felisme <jeazy@bizdotsmedia.com>
2023-03-30 16:44:03 -0400

    Create Page component

---
+++ pages/components/Page/Page.js
@@ -0,0 +1,15 @@
+import { MainMenu } from "../MainMenu/";
+import { BlockRenderer } from "../BlockRenderer";
+
+export const Page = (props) => {
+  return (
+    <div>
+      <MainMenu
+        items={props.mainMenuItems}
+        callToActionDestination={props.callToActionDestination}
+        callToActionLabel={props.callToActionLabel}
+      />
+      <BlockRenderer blocks={props.blocks} />
+    </div>
+  );
+};
