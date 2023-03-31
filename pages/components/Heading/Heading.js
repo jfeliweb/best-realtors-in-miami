015fb945cdbf6f9efbcab10bb4f8bb325d2c8569
Jean Felisme <jeazy@bizdotsmedia.com>
2023-03-31 14:07:00 -0400

    Fixing WP Heading block default issue.

--- pages/components/Heading/Heading.js
+++ pages/components/Heading/Heading.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { getFontSizeForHeading, getTextAlign } from "utils/fonts";
 
-export const Heading = ({ content, level, textAlign }) => {
+export const Heading = ({ content, level = 2, textAlign }) => {
   const tag = React.createElement(`h${level}`, {
     dangerouslySetInnerHTML: { __html: content },
     className: `font-heading max-w-5xl mx-auto my-5 ${getFontSizeForHeading(
